09470946a4fcd8cbfdcd3cc852b95db53325899d
Ozan Caglayan <ozancag@gmail.com>
2020-03-20 10:46:19 +0000

    update feature scripts

--- features/gen-remaining-1indexed.py
+++ features/gen-remaining-1indexed.py
@@ -3,17 +3,27 @@ import os
 
 IMG_PATH = "/data/ozan/datasets/conceptual_captions/raw_files/images"
 
-if __name__ == '__main__':
+def read_extracted_files():
     extracted = []
-    # Hacettepe finished these
-    with open('extracted_images') as f:
-        for line in f:
-            # convert 0-indices to 1-indices
-            extracted.append(int(line.strip()) + 1)
 
-    # get all image paths (starts at 1 not 0)
+    # 0-based filenames
+    files = [f for f in os.listdir('.') if f.endswith('.extracted')]
+    for fname in files:
+        with open(fname) as f:
+            for line in f:
+                # convert to 1-based
+                extracted.append(int(line.strip()) + 1)
+    return set(extracted)
+
+
+if __name__ == '__main__':
+    # (1-based)
+    extracted = read_extracted_files()
+
+    # get all image paths (1-based)
     all_imgs = [int(f) for f in os.listdir(IMG_PATH)]
-    remaining = sorted(list(set(all_imgs).difference(set(extracted))))
+
+    remaining = sorted(list(set(all_imgs).difference(extracted)))
 
     with open('remaining', 'w') as f:
         for line in remaining:
