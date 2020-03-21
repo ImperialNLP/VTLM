ae912f3e1dc77509690dc191071c2de95b7bfef2
Ozan Caglayan <ozancag@gmail.com>
2020-03-21 16:04:35 +0000

    update collect script

--- features/collect-extracted-icl.sh
+++ features/collect-extracted-icl.sh
@@ -3,4 +3,4 @@
 # 0-based files
 find /vol/bitbucket/ocaglaya/cc_feats/faster_rcnn_inception_resnet_v2_atrous_oid_v4_2018_12_12_ghconfig_reexport -type f | \
   sed 's/.*\/\([0-9]*\).pbz2/\1/' | sort -n > icl.extracted
-bzip2 -k icl.extracted
+bzip2 -f icl.extracted
