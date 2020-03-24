61940cf7f1fb0f626ad3a6dea71d47499f2132db
Ozan Caglayan <ozancag@gmail.com>
2020-03-24 17:15:47 +0000

    update

--- scripts/tf2_extractor_multigpu.sh
+++ scripts/tf2_extractor_multigpu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 for GPU in 0 1 2 3 5 6 7 8; do
-  CUDA_VISIBLE_DEVICES=$GPU python scripts/tf2_extractor.py -l features/remaining.ab.0${GPU} \
-    -f picklebz2 -o /data/ozan/datasets/conceptual_captions/features -O 1 -p &
+  CUDA_VISIBLE_DEVICES=$GPU python scripts/tf2_extractor.py -l features/remaining.0${GPU} \
+    -f picklebz2 -o /data/ozan/datasets/conceptual_captions/features \
+    -i /data2/ozan/conceptual_captions/images -O 1 -p &
 done
 wait
