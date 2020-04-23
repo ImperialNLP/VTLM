b2379662fb56eceaff20c54ceee43feeed51ccd3
Ozan Caglayan <ozancag@gmail.com>
2020-04-23 12:31:27 +0100

    bla.sh: if checkpoint does not exist, dont pass it

--- XLM-master/bla.sh
+++ XLM-master/bla.sh
@@ -9,6 +9,13 @@ export NGPU=${NGPU:-3}
 export DATA_PATH=${DATA_PATH:-/data/shared/ConceptualCaptions/XLM_data/50k}
 export DUMP_PATH=${DUMP_PATH:-/data/menekse/dumped}
 
+# Reloading
+RELOAD_FILE="${DUMP_PATH}/xlm_en_de_tlm/3094/best-valid_en_de_mlm_ppl.pth"
+RELOAD_MODEL_ARGS=""
+if [ -f $RELOAD_FILE ]; then
+  RELOAD_MODEL_ARGS="--reload_model ${RELOAD_FILE}"
+fi
+
 if [ $NGPU == "1" ]; then
     ipython -i train.py -- --exp_name xlm_en_de_tlm \
     --dump_path $DUMP_PATH --data_path $DATA_PATH \
@@ -16,8 +23,7 @@ if [ $NGPU == "1" ]; then
     --dropout 0.1 --attention_dropout 0.1 --gelu_activation true --batch_size 32 --bptt 256 \
     --optimizer adam,lr=0.0001 --epoch_size 300000 --max_epoch 100000 \
     --validation_metrics valid_en_de_mlm_ppl --stopping_criterion valid_en_de_mlm_ppl,25 \
-    --fp16 false --reload_model "${DUMP_PATH}/xlm_en_de_tlm/3094/best-valid_en_de_mlm_ppl.pth" \
-    --save_periodic 2
+    --fp16 false --save_periodic 2 ${RELOAD_MODEL_ARGS}
 else
   python -m torch.distributed.launch --nproc_per_node=$NGPU --nnodes=1 --node_rank=0 \
     --master_addr="193.140.236.52" --master_port=8088 train.py --exp_name xlm_en_de_tlm \
@@ -26,6 +32,5 @@ else
     --dropout 0.1 --attention_dropout 0.1 --gelu_activation true --batch_size 32 --bptt 256 \
     --optimizer adam,lr=0.0001 --epoch_size 300000 --max_epoch 100000 \
     --validation_metrics valid_en_de_mlm_ppl --stopping_criterion valid_en_de_mlm_ppl,25 \
-    --fp16 false --reload_model "${DUMP_PATH}/xlm_en_de_tlm/3094/best-valid_en_de_mlm_ppl.pth" \
-    --save_periodic 2
+    --fp16 false --save_periodic 2 ${RELOAD_MODEL_ARGS}
 fi
