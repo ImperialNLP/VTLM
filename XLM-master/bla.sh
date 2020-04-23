98e04bea224244767e7cd4f3de50867d877f2294
Ozan Caglayan <ozancag@gmail.com>
2020-04-23 13:08:41 +0100

    bla.sh: allow passing override args from cmdline

--- XLM-master/bla.sh
+++ XLM-master/bla.sh
@@ -23,7 +23,7 @@ if [ $NGPU == "1" ]; then
     --dropout 0.1 --attention_dropout 0.1 --gelu_activation true --batch_size 32 --bptt 256 \
     --optimizer adam,lr=0.0001 --epoch_size 300000 --max_epoch 100000 \
     --validation_metrics valid_en_de_mlm_ppl --stopping_criterion valid_en_de_mlm_ppl,25 \
-    --fp16 false --save_periodic 2 ${RELOAD_MODEL_ARGS}
+    --fp16 false --save_periodic 2 ${RELOAD_MODEL_ARGS} $@
 else
   python -m torch.distributed.launch --nproc_per_node=$NGPU --nnodes=1 --node_rank=0 \
     --master_addr="193.140.236.52" --master_port=8088 train.py --exp_name xlm_en_de_tlm \
@@ -32,5 +32,5 @@ else
     --dropout 0.1 --attention_dropout 0.1 --gelu_activation true --batch_size 32 --bptt 256 \
     --optimizer adam,lr=0.0001 --epoch_size 300000 --max_epoch 100000 \
     --validation_metrics valid_en_de_mlm_ppl --stopping_criterion valid_en_de_mlm_ppl,25 \
-    --fp16 false --save_periodic 2 ${RELOAD_MODEL_ARGS}
+    --fp16 false --save_periodic 2 ${RELOAD_MODEL_ARGS} $@
 fi
