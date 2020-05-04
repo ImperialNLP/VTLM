6fa510cf582d20868d44d33fc9e1ed49377e1dc3
Ozan Caglayan <ozancag@gmail.com>
2020-05-04 15:56:14 +0100

    dump-checkpoint-stats: print average L2 norms of weights

--- XLM-master/scripts/dump-checkpoint-stats.py
+++ XLM-master/scripts/dump-checkpoint-stats.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python
+from collections import defaultdict
+
 import sys
 import torch
 
 
 if __name__ == '__main__':
     for ckpt in sys.argv[1:]:
-        x = torch.load(ckpt)
-        print(f"{ckpt} {x['best_metrics']} -- best_stopping_criterion: {x['best_stopping_criterion']}")
+        x = torch.load(ckpt, map_location='cpu')
+        #print(f"{ckpt} {x['best_metrics']} -- best_stopping_criterion: {x['best_stopping_criterion']}")
+        if 'model' in x:
+            params = x['model']
+            norms = [('enc_' + n, p.norm(2).item()) for n, p in params.items()]
+        else:
+            enc_norms = [('enc_' + n, p.norm(2).item()) for n, p in x['encoder'].items()]
+            dec_norms = [('dec_' + n, p.norm(2).item()) for n, p in x['decoder'].items()]
+            norms = enc_norms + dec_norms
+
+        avg_norms = defaultdict(list)
+        for name, norm in norms:
+            if name.endswith('weight'):
+                avg_norms[name.split('.')[0]].append(norm)
+
+        avg_norms = [(name, sum(norms) / len(norms)) for name, norms in avg_norms.items()]
+        avg_norms = sorted(avg_norms, key=lambda x: x[1])
+
+        for (name, norm) in avg_norms:
+            print(f'{name:<40} avgnorm: {norm:10.2f}')
