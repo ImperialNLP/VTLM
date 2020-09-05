a9dc4ef410bd5b149d3925cba31ad658c0089a67
Ozan Caglayan <ozancag@gmail.com>
2020-09-05 12:36:31 +0100

    scripts: update

--- XLM-master/scripts/train-multi30k.sh
+++ XLM-master/scripts/train-multi30k.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 DATA_PATH=/data2/ozan/conceptual_captions/mmvc_icl_data/parallel.tok.bpe/multi30k
-DUMP_PATH=/data/ozan/experiments/xlm_mmvc_multi30k_scratch
-
-NAME=$1
-
-if [ -z $NAME ]; then
-  echo 'provide a name for the experiment'
-  exit 1
-fi
-shift 1
+DUMP_PATH=/data/ozan/experiments/mmvc/mmvc_code/multi30k_from_scratch
 
 CUR_DIR=`dirname $0`
 TRAIN=`realpath ${CUR_DIR}/../train.py`
@@ -19,10 +11,10 @@ EPOCH=`wc -l ${DATA_PATH}/train.${PAIR}.$L1 | head -n1 | cut -d' ' -f1`
 
 CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0}
 
-python $TRAIN --beam_size 1 --exp_name ${NAME} --dump_path ${DUMP_PATH} \
+python $TRAIN --beam_size 1 --exp_name "nmt_512d_lr0.0001" --dump_path ${DUMP_PATH} \
   --data_path ${DATA_PATH} --encoder_only false \
-  --lgs 'en-de' --mt_step "en-de" $PREV_ARGS \
-  --dropout '0.1' --attention_dropout '0.1' --gelu_activation true \
-  --batch_size 64 --optimizer "adam,lr=0.00001" \
-  --epoch_size ${EPOCH} --eval_bleu true --max_epoch 50 \
-  --stopping_criterion 'valid_en-de_mt_bleu,10' --validation_metrics 'valid_en-de_mt_bleu' $@
+  --lgs 'en-de' --mt_step "en-de" --emb_dim 512 --n_layers 6 --n_heads 8 \
+  --dropout '0.2' --attention_dropout '0.1' --gelu_activation true \
+  --batch_size 64 --optimizer "adam,lr=0.0001" \
+  --epoch_size ${EPOCH} --eval_bleu true --max_epoch 500 \
+  --stopping_criterion 'valid_en-de_mt_bleu,20' --validation_metrics 'valid_en-de_mt_bleu' $@
