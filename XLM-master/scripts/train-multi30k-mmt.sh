81845cd14d0edccb4a61093d095547b7e2881c50
Ozan Caglayan <ozancag@gmail.com>
2020-09-13 20:11:42 +0100

    update scripts

--- XLM-master/scripts/train-multi30k-mmt.sh
+++ XLM-master/scripts/train-multi30k-mmt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DATA_PATH=/data2/ozan/conceptual_captions/mmvc_icl_data/parallel.tok.bpe/multi30k
-DUMP_PATH=/data/ozan/experiments/mmvc/mmvc_code/multi30k_from_scratch_v2
+DUMP_PATH=/data/ozan/experiments/mmvc/mmvc_code/multi30k_from_scratch_v3_do0.4_isqrtw6k
 FEAT_PATH=/data/ozan/datasets/multi30k/features/oidv4/avgpool
 
 CUR_DIR=`dirname $0`
@@ -12,11 +12,11 @@ EPOCH=`wc -l ${DATA_PATH}/train.${PAIR}.$L1 | head -n1 | cut -d' ' -f1`
 
 CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0}
 
-python $TRAIN --beam_size 1 --exp_name mmt_lr0.0001 --dump_path ${DUMP_PATH} \
+ipython -i $TRAIN -- --beam_size 1 --exp_name mmt --dump_path ${DUMP_PATH} \
   --data_path ${DATA_PATH} --encoder_only false \
   --lgs 'en-de' --mmt_step "en-de" --emb_dim 512 --n_layers 6 --n_heads 8 \
   --dropout '0.4' --attention_dropout '0.1' --gelu_activation true \
-  --batch_size 64 --optimizer "adam,lr=0.0001" \
+  --batch_size 64 --optimizer "adam_inverse_sqrt,beta1=0.9,beta2=0.98,lr=0.0001,warmup_updates=6000" \
   --epoch_size ${EPOCH} --eval_bleu true --max_epoch 500 \
   --stopping_criterion 'valid_en-de_mmt_bleu,20' --validation_metrics 'valid_en-de_mmt_bleu' \
   --region_feats_path $FEAT_PATH --image_names ${DATA_PATH} $@
--- XLM-master/scripts/train-multi30k-nmt-small.sh
+++ XLM-master/scripts/train-multi30k-nmt-small.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DATA_PATH=/data2/ozan/conceptual_captions/mmvc_icl_data/parallel.tok.bpe/multi30k
-DUMP_PATH=/data/ozan/experiments/mmvc/mmvc_code/multi30k_from_scratch_v3_do0.4_isqrtw4k
+DUMP_PATH=/data/ozan/experiments/mmvc/mmvc_code/multi30k_from_scratch_v3_do0.4_isqrtw4k_small
 
 CUR_DIR=`dirname $0`
 TRAIN=`realpath ${CUR_DIR}/../train.py`
@@ -13,8 +13,8 @@ CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0}
 
 python $TRAIN --beam_size 1 --exp_name nmt --dump_path ${DUMP_PATH} \
   --data_path ${DATA_PATH} --encoder_only false \
-  --lgs 'en-de' --mt_step "en-de" --emb_dim 512 --n_layers 6 --n_heads 8 \
-  --dropout '0.4' --attention_dropout '0.1' --gelu_activation true \
+  --lgs 'en-de' --mt_step "en-de" --emb_dim 512 --n_layers 5 --n_heads 2 \
+  --dropout '0.4' --attention_dropout '0.2' --gelu_activation true \
   --batch_size 64 --optimizer "adam_inverse_sqrt,beta1=0.9,beta2=0.98,lr=0.0001,warmup_updates=4000" \
   --epoch_size ${EPOCH} --eval_bleu true --max_epoch 500 \
   --stopping_criterion 'valid_en-de_mt_bleu,20' --validation_metrics 'valid_en-de_mt_bleu' $@
