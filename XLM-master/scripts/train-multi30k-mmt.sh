7daaceeb05524f7e6c315895254636c759a5773e
Ozan Caglayan <ozancag@gmail.com>
2020-09-15 12:11:43 +0100

    update scripts

--- XLM-master/scripts/train-multi30k-mmt.sh
+++ XLM-master/scripts/train-multi30k-mmt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DATA_PATH=/data2/ozan/conceptual_captions/mmvc_icl_data/parallel.tok.bpe/multi30k
-DUMP_PATH=/data/ozan/experiments/mmvc/mmvc_code/multi30k_from_scratch_v3_do0.4_isqrtw4k
+DUMP_PATH=/data/ozan/experiments/mmvc/mmvc_code/multi30k_from_scratch_v4_do0.4
 FEAT_PATH=/data/ozan/datasets/multi30k/features/oidv4/avgpool
 
 CUR_DIR=`dirname $0`
@@ -12,7 +12,7 @@ EPOCH=`wc -l ${DATA_PATH}/train.${PAIR}.$L1 | head -n1 | cut -d' ' -f1`
 
 CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES:-0}
 
-ipython -i $TRAIN -- --beam_size 1 --exp_name mmt --dump_path ${DUMP_PATH} \
+python $TRAIN --beam_size 1 --exp_name mmt --dump_path ${DUMP_PATH} \
   --data_path ${DATA_PATH} --encoder_only false \
   --lgs 'en-de' --mmt_step "en-de" --emb_dim 512 --n_layers 6 --n_heads 8 \
   --dropout '0.4' --attention_dropout '0.1' --gelu_activation true \
