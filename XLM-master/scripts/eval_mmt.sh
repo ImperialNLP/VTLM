7249045b293a1fb92aebda78eb26049ceefe921e
Ozan Caglayan <ozancag@gmail.com>
2020-09-22 10:14:47 +0100

    update scripts

--- XLM-master/scripts/eval_mmt.sh
+++ XLM-master/scripts/eval_mmt.sh
@@ -24,7 +24,7 @@ for TEST_SET in test_2016_flickr test_2017_flickr test_2017_mscoco test_2018_fli
   python $TRAIN --beam_size ${BS} --exp_name $NAME --exp_id "${TEST_SET}" --dump_path $DUMP_PATH \
     --reload_model "${CKPT},${CKPT}" --data_path "${DATA_PATH}_${TEST_SET}" \
     --encoder_only false --lgs 'en-de' --mmt_step "en-de" --emb_dim 512 --n_layers 6 --n_heads 8 \
-    --dropout '0.2' --attention_dropout '0.1' --gelu_activation true \
+    --dropout '0.4' --attention_dropout '0.1' --gelu_activation true \
     --batch_size 64 --optimizer "adam,lr=0.0001" \
     --eval_bleu true --eval_only true \
     --region_feats_path $FEAT_PATH --image_names ${DATA_PATH}_${TEST_SET} $@
