0b96b29390de4e3d453289bd6afa23f0d371276e
Ozan Caglayan <ozancag@gmail.com>
2020-10-08 10:29:29 +0100

    update scripts

--- XLM-master/scripts/eval_mmt.sh
+++ XLM-master/scripts/eval_mmt.sh
@@ -20,12 +20,12 @@ TRAIN=`realpath ${CUR_DIR}/../train.py`
 BS=${BS:-1}
 NAME="${CKPT/.pth/}_beam${BS}/"
 
-for TEST_SET in test_2016_flickr test_2017_flickr test_2017_mscoco test_2018_flickr; do
+for TEST_SET in test_2016_flickr test_2017_flickr test_2017_mscoco; do
   python $TRAIN --beam_size ${BS} --exp_name $NAME --exp_id "${TEST_SET}" --dump_path $DUMP_PATH \
     --reload_model "${CKPT},${CKPT}" --data_path "${DATA_PATH}_${TEST_SET}" \
     --encoder_only false --lgs 'en-de' --mmt_step "en-de" --emb_dim 512 --n_layers 6 --n_heads 8 \
     --dropout '0.4' --attention_dropout '0.1' --gelu_activation true \
     --batch_size 64 --optimizer "adam,lr=0.0001" \
-    --eval_bleu true --eval_only true \
+    --eval_bleu true --eval_only true --reg_enc_bias false --num_of_regions 36 \
     --region_feats_path $FEAT_PATH --image_names ${DATA_PATH}_${TEST_SET} $@
 done
